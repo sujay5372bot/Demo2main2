5c6d5c0d2bd3543e32ac2178e72244f4c9029187
sujay5372bot <sujay53721@gmail.com>
2026-01-14 18:15:50 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -774,7 +774,7 @@ async def send_all(bot, userid, files, ident, chat_id, user_name, query):
                                 InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
                                 InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                             ],[
-                                InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/Sorry_Sorry_Galti_Ho_Gai")
+                                InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="https://t.me/onefighterarmy")
                                 ]
                             ]
                         )
