deca2f4be9b4472fd473d391651c1859c42a39d1
sujay5372bot <sujay53721@gmail.com>
2026-01-14 23:40:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -176,9 +176,9 @@ else:
     ON_HEROKU = False
 HAS_SSL = bool(getenv('HAS_SSL', True))
 if HAS_SSL:
-    URL = "".format(FQDN)
+    URL = "https://joint-leelah-afexlegend-944eb40b.koyeb.app/".format(FQDN)
 else:
-    URL = "".format(FQDN)
+    URL = "https://joint-leelah-afexlegend-944eb40b.koyeb.app/".format(FQDN)
 
 # ============================
 # Reactions Configuration
