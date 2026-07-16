13a24affaa9abd7ee3418637f1c9cbddd414259c
sujay5372bot <sujay53721@gmail.com>
2026-07-16 20:27:22 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -2533,7 +2533,7 @@ async def ai_spell_check(chat_id, wrong_name):
         return
     for _ in range(5):
         closest_match = process.extractOne(wrong_name, movie_list)
-        if not closest_match or closest_match[1] <= 80:
+        if not closest_match or closest_match[1] <= 70:
             return 
         movie = closest_match[0]
         files, offset, total_results = await get_search_results(chat_id=chat_id, query=movie)
